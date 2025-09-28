5d31bea2d29557d5f35645842dd925529c863e2e
Tien6699 <nguyendangnhattien4@gmail.com>
2025-09-28 22:31:53 +0700

    Add files via upload

---
+++ Lab3.2_Json/StudentInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._2_Json
+{
+    public class StudentInfo
+    {
+        public string MSSV {  get; set; }
+        public string Hoten { get; set; }
+        public int Tuoi { get; set; }
+        public double Diem {  get; set; }
+        public bool TonGiao { get; set;}
+
+        public StudentInfo(string mssv, string hoten, int tuoi, double diem, bool tongiao)
+        {
+            this.MSSV = mssv;
+            this.Hoten = hoten;
+            this.Tuoi = tuoi;
+            this.Diem = diem;
+            this.TonGiao = tongiao;
+        }
+
+    }
+}
